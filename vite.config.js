a8b58f9545f2bb77825a9f395387bb408e1c31db
Amandeep singh <151811507+bisrai@users.noreply.github.com>
2024-05-23 23:19:01 +0530

    add new file

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import vitePluginSvgr from 'vite-plugin-svgr'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    outDir: "build"
+  },
+  base: "/Portfolio-Website/",
+  plugins: [
+    react(),
+    vitePluginSvgr({
+      svgrOptions: {
+        icon: true,
+      }
+    })
+  ],
+})
