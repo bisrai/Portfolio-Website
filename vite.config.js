e5b60c811dff8f9ede59fa077d13ee445e1fc717
Amandeep Singh <bisrai06@gmail.com>
2024-05-27 22:00:05 +0530

    inital change

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import vitePluginSvgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "build"
-  },
   base: "/Portfolio-Website/",
   plugins: [
     react(),
